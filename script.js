7284dd12bcf18fa2479cfe8a02c0b7bc4cd71a97
Bizz Michelson <bizz.michelson@gmail.com>
2017-10-11 16:16:06 -0500

    working calculate

--- script.js
+++ script.js
@@ -1,45 +1,82 @@
-var atticus = { name: "Atticus", employeeNumber: "2405", annualSalary: "47000", reviewRating: 3 };
-var jem = { name: "Jem", employeeNumber: "62347", annualSalary: "63500", reviewRating: 4 };
-var boo = { name: "Boo", employeeNumber: "11435", annualSalary: "54000", reviewRating: 3 };
-var scout = { name: "Scout", employeeNumber: "6243", annualSalary: "74750", reviewRating: 5 };
-var robert = { name: "Robert", employeeNumber: "26835", annualSalary: "66000", reviewRating: 1 };
-var mayella = { name: "Mayella", employeeNumber: "89068", annualSalary: "35000", reviewRating: 2 };
+
 
 var employees = [ atticus, jem, boo, scout, robert, mayella ];
 var payroll = [];
 
-function Calculate (name, bonusPercentage, totalCompensation, totalBonus) {
-   this.name = name;
-   this.bonusPercentage = bonusPercentage;
-   this.totalCompensation = totalCompensation;
-   this.totalBonus = totalBonus;
-};
+class Employees {
+    constructor(name, employeeNumber, annualSalary, reviewRating){
+        this.name = name;
+        this.employeeNumber = employeeNumber;
+        this.annualSalary = annualSalary;
+        this.reviewRating = reviewRating;
+    }
+    
 
-function findBonus(employeeArray) {
-    console.log(employeeArray);
-    for (var i = 0; i < employeeArray.length; i++) {
-        employeeArray[i].bonusPercentage = 0;
-      if (employeeArray[i].reviewRating == 5) {
-        employeeArray[i].bonusPercentage += 0.1;
-      } if (employeeArray[i].reviewRating == 4) {
-        employeeArray[i].bonusPercentage += 0.06;
-     } if (employeeArray[i].reviewRating == 3) {
-        employeeArray[i].bonusPercentage += 0.04; 
-    } if ( employees[i].employeeNumber.length == 4) {
-        employeeArray[i].bonusPercentage += .05;
-    } if ( parseInt( employees[i].annualSalary ) > 65000) {
-        employeeArray[i].bonusPercentage -= .01;        
-    } if (employees[i].bonusPercentage > 0.13) {
-        employeeArray[i].bonusPercentage = 0.13;
-        } else if (employeeArray[i].bonusPercentage < 0) {
-        employeeArray[i].bonusPercentage = 0;
-        } else {
-        employeeArray[i].bonusPercentage = employeeArray[i].bonusPercentage;
-        }
-    payroll.push(new Calculate (employeeArray[i].name, employeeArray[i].bonusPercentage, Math.round( ((employeeArray[i].bonusPercentage)+1) * employeeArray[i].annualSalary ) , Math.round((employeeArray[i].bonusPercentage * employeeArray[i].annualSalary) )) )        
-    }    
-    return payroll;
+    calculate (totalCompensation, totalBonus)  {
+        this.bonusPercentage = 0;
+        if (this.reviewRating == 5) {
+          this.bonusPercentage += 0.1;
+        } if (this.reviewRating == 4) {
+          this.bonusPercentage += 0.06;
+       } if (this.reviewRating == 3) {
+          this.bonusPercentage += 0.04; 
+      } if ( this.employeeNumber.length == 4) {
+          this.bonusPercentage += .05;
+      } if ( parseInt( this.annualSalary ) > 65000) {
+          this.bonusPercentage -= .01;        
+      } if (this.bonusPercentage > 0.13) {
+          this.bonusPercentage = 0.13;
+          } else if (this.bonusPercentage < 0) {
+              this.bonusPercentage = 0;
+          }
+    this.totalCompensation = Math.round( ((this.bonusPercentage)+1) * this.annualSalary );
+    this.totalBonus =  Math.round((this.bonusPercentage * this.annualSalary) );
+    }
 }
 
+var atticus = new Employees ("Atticus", "2405", 47000, 3);
+var jem = new Employees("Jem", "62347", 63500, 4);
+var boo = new Employees("Boo", "11435", 54000, 3);
+var scout = new Employees("Scout", "6243", 74750, 5);
+var robert = new Employees("Robert", "26835", 66000, 1 );
+var mayella = new Employees("Mayella", "89068",35000, 2 );
 
-console.log(findBonus(employees));
+
+
+// function Calculate (name, bonusPercentage, totalCompensation, totalBonus) {
+//    this.name = name;
+//    this.bonusPercentage = bonusPercentage;
+//    this.totalCompensation = totalCompensation;
+//    this.totalBonus = totalBonus;
+// };
+
+
+
+// function findBonus(employeeArray) {
+//     console.log(employeeArray);
+//     for (var i = 0; i < employeeArray.length; i++) {
+//         employeeArray[i].bonusPercentage = 0;
+//       if (employeeArray[i].reviewRating == 5) {
+//         employeeArray[i].bonusPercentage += 0.1;
+//       } if (employeeArray[i].reviewRating == 4) {
+//         employeeArray[i].bonusPercentage += 0.06;
+//      } if (employeeArray[i].reviewRating == 3) {
+//         employeeArray[i].bonusPercentage += 0.04; 
+//     } if ( employees[i].employeeNumber.length == 4) {
+//         employeeArray[i].bonusPercentage += .05;
+//     } if ( parseInt( employees[i].annualSalary ) > 65000) {
+//         employeeArray[i].bonusPercentage -= .01;        
+//     } if (employees[i].bonusPercentage > 0.13) {
+//         employeeArray[i].bonusPercentage = 0.13;
+//         } else if (employeeArray[i].bonusPercentage < 0) {
+//         employeeArray[i].bonusPercentage = 0;
+//         } else {
+//         employeeArray[i].bonusPercentage = employeeArray[i].bonusPercentage;
+//         }
+//     payroll.push(new Calculate       
+//     }    
+//     return payroll;
+// }
+
+
+// console.log(findBonus(employees));
